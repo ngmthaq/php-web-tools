e68dcbfbc08e86b4a95eabe36859647f21ee8e97
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-02-13 13:49:10 +0700

    Update script

--- tools/script/build-dev.sh
+++ tools/script/build-dev.sh
@@ -1,10 +1,12 @@
-PROD_FILE=/var/www/html/.prod
+#!/bin/sh
 
 composer install
 
 yarn install
 
 yarn dev
+
+PROD_FILE='/var/www/html/.prod'
 
 if [ -f "$PROD_FILE" ]; then
     rm $PROD_FILE
--- tools/script/build-prod.sh
+++ tools/script/build-prod.sh
@@ -1,10 +1,12 @@
-PROD_FILE=/var/www/html/.prod
+#!/bin/sh
 
 composer install
 
 yarn install
 
 yarn build
+
+PROD_FILE='/var/www/html/.prod'
 
 if [ -f "$PROD_FILE" ]; then
     rm $PROD_FILE
